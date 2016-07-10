f1aa10cdd3dd2ecdc2fdf4936f82e9db66b01ae1
kennymaccormak <volodymyr.chygura@gmail.com>
2016-07-10 15:42:46 +0300

    change animation

---
+++ js/menupop.js
@@ -0,0 +1,13 @@
+document.getElementById("first_a1").onmouseover = function () {
+    document.getElementById("first_a1").style.background = "url('img/nav/small_menu_icon.png')";
+    document.getElementById("first_a1").style.backgroundRepeat = "no-repeat";
+    document.getElementById("first_a1").style.backgroundPosition = "3px 3px";
+    document.getElementById("menu_pop").style.top = "45px";
+};
+
+document.getElementById("first_a1").onmouseout = function() {
+    document.getElementById("first_a1").style.background = "url('img/nav/menu_icon.png')";
+    document.getElementById("first_a1").style.backgroundRepeat = "no-repeat";
+    document.getElementById("first_a1").style.backgroundPosition = "top left";
+    document.getElementById("menu_pop").style.top = "-200px";
+}
